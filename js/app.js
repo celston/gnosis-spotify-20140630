101a3282b0be886a03da49fc0ddd6f052361ad1a
celston <celston@gmail.com>
2014-07-01 08:52:44 -0400

    synced Spotify and AngularJS routing/navigation

--- js/app.js
+++ js/app.js
@@ -1,26 +1,89 @@
-var app = angular.module('gnosisApp', []);
+var app = angular.module('gnosisApp', ['ngRoute']);
 
-require([
-    '$api/models',
-], function(models) {
+app.config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        })
+        .when('/foo', {
+            templateUrl: 'foo.html',
+            controller: 'FooController'
+        })
+        .when('/bar/:id', {
+          templateUrl: 'bar.html',
+          controller: 'BarController'
+        })
+        .otherwise({
+            redirectTo: function() {
+                console.log('check');
+                return '/home';
+            }
+        });
 
-    app.
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|sp):/);
+}]);
 
-    function navigation() {
-        var args = models.application.arguments;
-        if (args) {
-            var lastArg = args[args.length - 1];
-            if (lastArg !== 'index' && lastArg !== 'tabs') {
-                return;
+app.controller('NavController', ['$scope', '$location', function ($scope, $location) {
+    $scope.tabs = [
+        {
+            text: 'Home',
+            path: 'home'
+        },
+        {
+            text: 'Foo',
+            path: 'foo'
+        }
+    ];
+    $scope.isActive = function (path) {
+        return $location.path() == '/' + path;
+    }
+}])
+
+app.controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+    $scope.recentTracks = [];
+    $http.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=celston&api_key=59d09be6bab770f89ca6eeb33ae2b266&format=json').success(function (data) {
+        $scope.recentTracks = data.recenttracks.track;
+    });
+}]);
+
+app.controller('FooController', ['$scope', function ($scope) {
+}]);
+
+app.controller('BarController', ['$scope', '$routeParams', function ($scope, $routeParams) {
+    $scope.id = $routeParams.id;
+}]);
+
+app.run(function ($rootScope, $location) {
+    require([
+        '$api/models',
+    ], function(models) {
+
+        function navigation() {
+            var args = models.application.arguments;
+            console.log(args);
+            if (args) {
+                var lastArg = args[args.length - 1];
+                if (lastArg !== 'index' && lastArg !== 'tabs') {
+                    return;
+                }
+
+                if (lastArg == 'index') {
+                    args.pop();
+                    var path = args.join('/');
+
+                    $rootScope.$apply(function () {
+                        $location.path('/' + path);
+                    });
+                }
             }
         }
 
-        console.log(args);
-    }
+        // When application has loaded, run pages function
+        models.application.load('arguments').done(navigation);
 
-    // When application has loaded, run pages function
-    models.application.load('arguments').done(navigation);
+        // When arguments change, run pages function
+        models.application.addEventListener('arguments', navigation);
+    });
+})
 
-    // When arguments change, run pages function
-    models.application.addEventListener('arguments', navigation);
-});
